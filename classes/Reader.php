0503b8130075b0873dd475c2fad1877c1fd9a453
IF <fiyanov@csgo.com>
2018-10-15 11:34:25 +0600

    start

---
+++ classes/Reader.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IF
+ * Date: 15.10.2018
+ * Time: 11:31
+ */
+
+class Reader
+{
+
+}
---
+++ classes/Transformer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IF
+ * Date: 15.10.2018
+ * Time: 11:32
+ */
+
+class Transformer
+{
+
+}
